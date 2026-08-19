c898662ba713740b40c88981a75b27f0861a2ee3
robinchacko246 <robinchacko246@gmail.com>
2026-08-19 14:16:13 +0530

    explorign realestsate

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  runtime: 'edge',
-};
-
-export default async function handler(request: Request) {
-  // Dynamic import to avoid bundling issues at cold start
-  const { default: server } = await import('../dist/server/assets/server-CGzWFl9C.js' as any);
-  return server.fetch(request, {}, {});
-}
