53d640cc791b32c640237472ac73f5b470b44c11
robinchacko246 <robinchacko246@gmail.com>
2026-08-19 13:50:03 +0530

    Add Vercel deployment configuration and serverless handler

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+export const config = {
+  runtime: 'edge',
+};
+
+import server from '../src/server';
+
+export default async function handler(request: Request) {
+  return server.fetch(request, {}, {});
+}
